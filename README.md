55d5abbabf56dfcb0655d941b5297f5703c1c745
Shadow15510 <antoine.royer14@orange.fr>
2023-07-20 14:36:10 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SCREW
-A Python module to manupulate Screws and Coscrews with geometrics algebra (Clifford's Algebra).
+# SCREW (Screw and Coscrew Research for Engineering World)
+[![Licence](https://img.shields.io/github/license/Shadow15510/SCREW?color=green)](https://github.com/Shadow15510/SCREW/blob/master/LICENSE)
+
+## Description
+A Python module to manipulate Screws and Coscrews with geometrics algebra (Clifford's Algebra).
+
+## Exemples
+First of all, you need to import the modules:
+```
+import geometric_algebra as ga
+from screw import Screw
+```
+The `screw` module also provides a `CoScrew` object and the `comoment` function for calculating the comoment between a coscrew and a screw.
+
+Once these modules have been imported, we can create the geometric algebra in which we will be working. For basic physical applications, a three-dimensionnal algebra should suffice:
+```
+my_algebra = ga.GeometricAlgebra(3)
+locals().update(my_algebra.blades)
+```
+The second line adds the basis blades to the local variables so that we will be able to create new multivectors just by performing linear combinations of these basis blades. For a 3D algebra, the basis blades are: 1, e1, e2, e3, e12, e13, e32, e123.
+
+We can now start working with Screw and CoScrew classes:
+```
+S = 1 + (2*e2) + (3*e3)  # the direction of the screw
+M = (e1) + (5*e3)        # the moment of the screw
+my_screw = Screw(S, M)
+```
+
+## Licence
+All the code is provided under the GNU General Public Licence v3.0+ (GPLv3+)
---
+++ src/screw.py
@@ -0,0 +1,208 @@
+"""
+This module provides two classes that respectively implement a screw and a co-screw.
+
+Exemples
+--------
+Before using theses objects, you should import the ``geometric_algebra`` module so that you can
+create multivectors. For basic physical applications, a three-dimensional should be enough, but you
+can use n-dimensional multivectors.
+
+Let's see a minimal exemple::
+
+    >>> import geometric_algebra as ga
+    >>> from screw import Screw
+    >>> my_algebra = ga.GeometricAlgebra(3)  # a 3-D geometric algebra
+    >>> locals().update(my_algebra.blades)   # add the basis blades to the locals (i.e. 1, e1, e2…)
+    >>> direction = 2 + (3*e1) + (6*e3)      # creates a MultiVector for the screw's direction
+    >>> moment = (2*e1) + (5*e2) + e3        # creates another MultiVector for the screw's moment
+    >>> my_screw = Screw(direction, moment)  # finally we create a Screw instance
+"""
+
+
+class Screw:
+    """Screw object
+
+    Attributes
+    ----------
+    direction : MultiVector
+        The direction multivector S or the screw.
+    moment : MultiVector
+        The moment multivector M of the the screw.
+    """
+    def __init__(self, direction, moment):
+        """Constructor method.
+
+        Parameters
+        ----------
+        direction : MultiVector
+            The direction of the screw, usually named S.
+        moment : MultiVector
+            The moment of the screw, usually named M.
+        """
+        self.direction = direction
+        self.moment = moment
+
+    def __repr__(self):
+        """Allow to display the Screw.
+
+        Returns
+        -------
+        out : str
+            The string representation of the Screw.
+        """
+        return f"Screw(\n\tdirection={self.direction}\n\tmoment={self.moment}\n)"
+
+    def __add__(self, other):
+        """The addition ``self + other``.
+
+        Parameters
+        ----------
+        other : Screw
+            The screw to be add up.
+
+        Returns
+        -------
+        out : Screw
+            The result of the addition of the two screws.
+
+        Raises
+        ------
+        TypeError
+            If ``other`` isn't a Screw.
+        """
+        if not isinstance(other, Screw):
+            raise TypeError(f"other must be a Screw instance instead of {type(other)}")
+
+        return Screw(
+                self.direction + other.direction,
+                self.moment + other.moment
+            )
+
+    def __xor__(self, other):
+        """The wedge product ``self ^ other``.
+        
+        Parameters
+        ----------
+        other : Screw
+            The other Screw.
+
+        Returns
+        -------
+        out : Screw
+            The result of the wedge product between the two given screws.
+
+        Raises
+        ------
+        TypeError
+            If ``other`` isn't a Screw.
+        """
+        if not isinstance(other, Screw):
+            raise TypeError
+        return Screw(
+                self.direction ^ other.moment + self.moment.gradeInvol() ^ other.direction,
+                self.moment ^ other.moment
+            )
+
+
+class CoScrew:
+    """Coscrew object
+
+    Attributes
+    ----------
+    direction : MultiVector
+        The direction multivector S or the coscrew.
+    moment : MultiVector
+        The moment multivector M of the the coscrew.
+    """
+    def __init__(self, direction, moment):
+        """Constructor method.
+
+        Parameters
+        ----------
+        direction : MultiVector
+            The direction of the screw, usually named S.
+        moment : MultiVector
+            The moment of the screw, usually named M.
+        """
+        self.direction = direction
+        self.moment = moment
+
+    def __repr__(self):
+        """Allow to display the CoScrew.
+
+        Returns
+        -------
+        out : str
+            The string representation of the CoScrew.
+        """
+        return f"CoScrew(\n\tdirection={self.direction}\n\tmoment={self.moment}\n)"
+
+    def __add__(self, other):
+        """The addition ``self + other``.
+
+        Parameters
+        ----------
+        other : CoScrew
+            The coscrew to be add up.
+
+        Returns
+        -------
+        out : CoScrew
+            The result of the addition of the two coscrews.
+
+        Raises
+        ------
+        TypeError
+            If ``other`` isn't a CoScrew.
+        """
+        if not isinstance(other, CoScrew):
+            raise TypeError(f"other must be a CoScrew instance instead of {type(other)}")
+        return CoScrew(
+                self.direction + other.direction,
+                self.moment + other.moment
+            )
+
+    def __rmul__(self, scalar):
+        """The right-hand multiplication between a coscrew and a scalar ``scalar * self``.
+
+        Parameters
+        ----------
+        scalar : int, float
+            The scalar to multiply.
+
+        Returns
+        -------
+        out : CoScrew
+            The result of the addition of the two coscrews.
+
+        Raises
+        ------
+        TypeError
+            If ``scalar`` isn't a scalar.
+        """
+        if not isinstance(scalar, (int, float)):
+            raise TypeError(f"scalar must be a scalar instead of {type(scalar)}")
+
+        return CoScrew(
+                scalar * self.direction,
+                scalar * self.moment
+            )
+
+
+def comoment(coscrew: CoScrew, screw: Screw):
+    """Compute the comoment between a coscrew and a screw.
+
+    Parameters
+    ----------
+    coscrew : CoScrew
+        The coscrew.
+    screw : Screw
+        The screw.
+
+    Returns
+    -------
+    out : MultiVector
+        The comoment between the given coscrew and the screw.
+    """
+    return (-coscrew.direction.gradeInvol() * screw.moment.gradeInvol()
+            + screw.direction * coscrew.moment)
